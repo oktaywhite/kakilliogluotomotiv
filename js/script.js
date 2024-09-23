776db32f41265362c2a5026fee6fcb95b5dec177
Oktay <oktaywhite@hotmail.com>
2024-09-23 21:55:37 +0300

    asd

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const mybutton = document.getElementById("goTopBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -14,3 +14,20 @@ function goToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//
+
+const mobileNavbar = document.getElementById('mobile-navbar');
+const hideBar = document.getElementById('bar-hide');
+const hideBarMobile = document.getElementById('bar-hide-mobile');
+
+hideBar.addEventListener('click', function () {
+  mobileNavbar.classList.remove('mobile-hidden');
+  mobileNavbar.classList.add('mobile-show');
+})
+
+hideBarMobile.addEventListener('click', function () {
+  mobileNavbar.classList.remove('mobile-show');
+  mobileNavbar.classList.add('mobile-hidden');
+})
+
